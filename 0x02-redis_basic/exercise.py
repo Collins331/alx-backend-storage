b73fce835b671e515a71305daeb47a631b251a29
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-15 22:27:38 +0300

    update exercise.py 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Main file that imports
 redis and uuid modules
 """
 import redis
-import uuid
+from uuid import uuid4
 from typing import Union
 
 
@@ -14,15 +14,17 @@ class Cache:
     """
     def __init__(self):
         """
-        Constructor
+        Constructor that initializes
+        the redis instance
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Store method
+        Store method that takes
+        data and returns a key
         """
-        key = str(uuid.uuid4())
+        key = str(uuid4())
         self._redis.set(key, data)
         return key
