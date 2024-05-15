eab485f69c3cf97a1ab361b09137bc9fd7f27215
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-15 22:51:08 +0300

    get string && int

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ redis and uuid modules
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -28,3 +28,30 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    bytes,
+                                                                    int,
+                                                                    float]:
+        """
+        Get method that takes a key
+        and an optional Callable function
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get method that takes a key
+        and returns a string
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Get method that takes a key
+        and returns an integer
+        """
+        return self.get(key, int)
